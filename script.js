471b67a26d6b18658f1829b1f8d04ce590290cb4
Lana Maidenbaum <lanamaidenbaum@Lanas-MacBook-Pro.local>
2017-09-03 15:31:30 +0300

    updated using modules

--- script.js
+++ script.js
@@ -1,61 +1,65 @@
-var beers = [];
-//peoject exercise 1
-function addBeer(name,category,rating){
-    var beer = {
-        name: name ,
-        category: category,
-        rating: rating
-    };
-    beers.push(beer);
-    updateBeers();
+var BeerAppReview = function(){
+    var beers = [];
+    function addBeer(name,category,rating){
+        var beer = {
+            name: name ,
+            category: category,
+            rating: rating
+        };
+        beers.push(beer);
+        updateBeers();
+    }
+
+    function updateBeers(){
+        $('.beers-list').empty();
+    
+        beers.forEach(function(beer){
+            $('.beers-list').append('<li>'+beer.name+': '+beer.category+' ('+beer.rating+') </li>');
+        });
+    }
+
+    function reverseListOrder(){
+        beers.reverse();
+        updateBeers();
+    }
+
+    function sortBeers(){
+        beers.sort(function(a,b){
+            if(a.name > b.name){
+                return 1;
+            } else if(a.name < b.name){
+                return -1;
+            } else {
+                return 0;
+            }
+        });
+        updateBeers();
+    }
+    return {
+        addBeer: addBeer,
+        updateBeers: updateBeers,
+        sortBeers: sortBeers,
+        reverseListOrder: reverseListOrder
+    }
 }
+var beerAppReview = BeerAppReview();
 
 $('.post-beer').click(function(){
     var beerName = $('.beer-input').val();
     var beerCategory = $('.category-input').val();
     var beerRating = $('.rating').val();
 
-    addBeer(beerName,beerCategory,beerRating);
+    beerAppReview.addBeer(beerName,beerCategory,beerRating);
 
     $('.beer-input').val('');
     $('.category-input').val('');
     $('.rating').val('');
 });
 
+$('#sortButton').click(beerAppReview.sortBeers);
 
-
-//project exercise 3
-function updateBeers(){
-    $('.beers-list').empty();
-
-    beers.forEach(function(beer){
-        $('.beers-list').append('<li>'+beer.name+': '+beer.category+' ('+beer.rating+') </li>');
-    });
-}
-
-function sortBeers(){
-    beers.sort(function(a,b){
-        if(a.name > b.name){
-            return 1;
-        } else if(a.name < b.name){
-            return -1;
-        } else {
-            return 0;
-        }
-    });
-    updateBeers();
-}
-$('#sortButton').click(sortBeers);
-
-//project exercise 4 extension - add the rating section
-//project exercise 5 extension - add a function to reverse the order of the posted beers
-function reverseListOrder(){
-    beers.reverse();
-    updateBeers();
-}
-$('#reverseButton').click(reverseListOrder);
+$('#reverseButton').click(beerAppReview.reverseListOrder);
 
 $('#clearButton').click(function(){
     $('.beers-list').empty()}
 );
-    
